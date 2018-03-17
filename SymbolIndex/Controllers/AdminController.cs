4f1f7cbcedb5cc14e1088cb627f6a99452440510
Khang Dinh Hoang <taigama@users.noreply.github.com>
2018-03-17 11:01:16 +0700

    bố fix rồi, bootstrap mới như ***

--- SymbolIndex/Controllers/AdminController.cs
+++ SymbolIndex/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,6 +24,76 @@ namespace SymbolIndex.Controllers
             return View();
         }
 
+        public ActionResult FontList()
+        {
+            return View(db.Fonts.ToList());
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult UploadFiles()
+        {
+            // Checking no of files injected in Request object  
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        //string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";  
+                        //string filename = Path.GetFileName(Request.Files[i].FileName);  
+
+                        HttpPostedFileBase file = files[i];
+                        string fname = String.Empty;
+                        string fpath = String.Empty;
+                        string fstorePath = String.Empty;
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+
+                        // Get the complete folder path and store the file inside it.  
+
+                        do
+                        {
+                            fpath = RandomString(10) + fname;
+                            fstorePath = Path.Combine(Server.MapPath("~/fonts/"), fpath);
+                            fpath = @"~/UploadFiles/" + fpath;
+                        } while (System.IO.File.Exists(fstorePath));
+
+                        file.SaveAs(fstorePath);
+                    }
+
+                    
+                    return Json("fpath", JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    return Json("SOMETHING_NOT_RIGHT", JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                return Json("OK", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
 
         public IDictionary<int, ushort> ListFont(int? fontId)
         {
