fc1bbc00f701182eb17b4b6e582aea1b34ead322
Khang Dinh Hoang <taigama@users.noreply.github.com>
2018-03-20 13:40:54 +0700

    api-able

---
+++ SymbolIndex/Controllers/APIController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web.Mvc;
+using SymbolIndex.Data;
+using SymbolIndex.Models;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SymbolIndex.Controllers
+{
+    public class APIController : Controller
+    {
+        SIContext db = new SIContext();
+
+        //[Route("API/GetFontInfo/{id}")]
+        [HttpGet]
+        public ActionResult GetFontInfo(int id, bool more = false)
+        {
+
+            Font theFont = db.Fonts.Find(id);
+            if(!more)
+            {
+                theFont.Symbols = null;
+            }
+            return ParseJson(theFont, more ? 3:1);
+        }
+
+        [HttpGet]
+        public ActionResult GetOverall()
+        {
+            object data = new
+            {
+                Fonts = db.Fonts.ToList(),
+                Symbols = db.Symbols.ToList(),
+                Tags = db.Tags.ToList(),
+                TagSymbols = db.Database.SqlQuery<TagSymbol>("select Tag_Id,Symbol_Id from dbo.TagSymbol")
+            };
+            
+            return ParseJson(data, 2);
+        }
+
+        [HttpGet]
+        public ActionResult GetTable(string id)
+        {
+            object data = null;
+            switch (id)
+            {
+                case "fonts":
+                    var fdata = db.Fonts.ToList();
+                    foreach(var font in fdata)
+                    {
+                        font.Symbols = null;
+                    }
+                    data = fdata;
+                break;
+                case "tags":
+                    var tdata = db.Tags.ToList();
+                    foreach (var tag in tdata)
+                    {
+                        tag.Symbols = null;
+                    }
+                    data = tdata;
+                    break;
+                default:
+                    break;
+            }
+
+            if (data == null)
+                return HttpNotFound("This table are not avaiable");
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonNetResult ParseJson(object data, int depth = 1
+            , ReferenceLoopHandling referenceLoopHandling = ReferenceLoopHandling.Ignore)
+        {
+            var result = new JsonNetResult
+            {
+                Data = data,
+                JsonRequestBehavior = System.Web.Mvc.JsonRequestBehavior.AllowGet,
+                Settings = {
+            ReferenceLoopHandling = referenceLoopHandling,
+            MaxDepth = depth// product, productdetail, author
+                }
+            };
+            return result;
+        }
+    }
+}
---
+++ SymbolIndex/Models/TagSymbol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SymbolIndex.Models
+{
+    [System.Serializable]
+    public class TagSymbol
+    {
+        public int Tag_Id { get; set; }
+        public int Symbol_Id { get; set; }
+    }
+}
